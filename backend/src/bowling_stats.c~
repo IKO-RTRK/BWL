cb83761e55d17ab31dfff89ee8ed1cc8d6e6eec9
Koncar93 <stefan.koncar@gmail.com>
2015-08-31 22:00:04 +0200

    Dodan makefile i folderi tests i build

---
+++ backend/src/bowling_stats.c~
@@ -0,0 +1,10 @@
+#include "bowling_stats.h"
+
+int skor()
+{
+  return 0;
+}
+int srusi(int x)
+{
+  return 0;
+}
---
+++ backend/tests/bowling_test.c
@@ -0,0 +1,11 @@
+#include "../unity/unity_fixture.h"
+#include "../src/bowling_stats.h"
+
+TEST_GROUP(BowlingTest);
+
+TEST_GROUP_RUNNER(BowlingTest)
+{
+	
+
+}
+
