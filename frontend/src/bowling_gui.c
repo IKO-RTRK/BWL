1ce2c445ae76659bc4d31090dffe4072638c40d0
BrankaPekez <branka.pekez.93@gmail.com>
2015-08-31 23:03:34 +0200

    dodani komentari

--- frontend/src/bowling_gui.c
+++ frontend/src/bowling_gui.c
@@ -7,9 +7,9 @@ void initialisation(void)
 {
     int curr_row;
     int curr_column;
-    int counter = NUM_OF_PIN - 1;
+    int counter = NUM_OF_PINS - 1;
     
-    
+    //unos staze
     for (curr_row = 0; curr_row < ROW; curr_row++)
     {
 	matrix[curr_row][0] = '|';
@@ -20,7 +20,7 @@ void initialisation(void)
 
     
     int tmp = 0, tmp1;
-    int n = 5;
+    int n = START_PINS;
 
     //unosenje cunjeva u matricu
     for (curr_row = 4; curr_row > 0; curr_row--)
@@ -35,11 +35,12 @@ void initialisation(void)
 	n++;
     }
 
-    matrix[BALL_POS_ROW][BALL_POS_COL] = bowling_ball;
+    
+    matrix[BALL_POS_ROW][BALL_POS_COL] = bowling_ball;  //pocetna pozicija na kojoj ce se nalaziti kugla
 
-    tmp = 22;
-    tmp1 = 23;
-
+    //prikaz rezultata
+    
+    tmp1 = START_LANE_ROW;				//prvi red od koga pocinju da se upisuju -
     for (curr_row = 0; curr_row < 10; curr_row++)
     {
 	for (curr_column = 0; curr_column < 3; curr_column++) 
@@ -50,6 +51,9 @@ void initialisation(void)
 
 	tmp1+=4;
     }
+    
+    
+    tmp = START_LANE_COLON;				//prva kolona od koje pocinju da se upisuju |
 
     for (curr_row = 0; curr_row < 11; curr_row++)
     {
@@ -60,10 +64,13 @@ void initialisation(void)
 	}
 	tmp+=4;
     }
-
+    
+    //primjer kako ce izgledati rezultati
+    
     matrix[1][23] = '_';
     matrix[1][25] = '1';
     matrix[2][25] = '1';
+    
     system("clear");
     print();
     usleep(700000);
@@ -72,16 +79,16 @@ void initialisation(void)
 
 void move(void)
 { 
-    int offset;
-    int curr_pos_row = BALL_POS_ROW;
-    int curr_pos_col = BALL_POS_COL;
-    int prev_pos_row;
-    int prev_pos_col;
+    int offset; 					//rezultat random-a, ukoliko kugla nije otisla van staze
+    int curr_pos_row = BALL_POS_ROW;			//trentuni red u kome se nalazi kugla u toku kretanja
+    int curr_pos_col = BALL_POS_COL;			//trentuna kolona u kojoj se nalazi kugla u toku kretanja
+    int prev_pos_row;					//prethodni red u kome se nalazila kugla
+    int prev_pos_col;					//prethodna kolona
     double a, b;		// koriste se samo za funkciju foo1
-    while (curr_pos_row >= 5)
+    while (curr_pos_row >= 5)				//sve dok ne dodje kugla do cunjeva
     {
-      if (curr_pos_col < 2 || curr_pos_col > 14)
-	offset = 0;
+      if (curr_pos_col < 2 || curr_pos_col > 14) 	//ako je kugla dosla do kraja staze, da se nastavi kretati pravo kroz kanal
+	offset = 0;					//ne koristi se vise radnom jer je njeno kretanje fiksno 
       else
       {
 	foo1(a,b); //napisao sam je samo da bih mogao dobiti bolje vrijednosti za random
@@ -99,8 +106,8 @@ void move(void)
       curr_pos_row -= 1;
       curr_pos_col = curr_pos_col + offset;
 	
-      matrix[prev_pos_row][prev_pos_col] = '.';
-      matrix[curr_pos_row][curr_pos_col] = bowling_ball;
+      matrix[prev_pos_row][prev_pos_col] = '.';		//na prethodnoj poziciji na kojoj se nalazila kugla upisi .
+      matrix[curr_pos_row][curr_pos_col] = bowling_ball;//na trenutnu poziciju na kojoj se nalazi kugla ucrtaj kuglu 
       system("clear");
       print();
       usleep(700000);
