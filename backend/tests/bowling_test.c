641a2d321c838b9f4b14b5c9c8fac8e48a5f3820
GogicM <milan.gogic89@gmail.com>
2015-09-02 15:01:09 -0700

    Dodana helper funkcija u testovima

--- backend/tests/bowling_test.c
+++ backend/tests/bowling_test.c
@@ -1,7 +1,14 @@
 #include "../unity/unity_fixture.h"
 #include "../src/bowling_stats.h"
 
-
+static void rollMany(int n, int pins) // helper funkcija umjesto beskonacnog pisanja iste for petlje :)
+{
+	int i;
+	for(i = 0; i < n; i++)
+	{
+	knockDown(pins);
+	}
+}
 TEST_GROUP(BowlingTest);
 
 TEST_GROUP_RUNNER(BowlingTest)
@@ -31,71 +38,53 @@ TEST_TEAR_DOWN(BowlingTest)
 // Prvi test - promasena sva bacanja
 TEST(BowlingTest, TestAllZeroes)
 {
-	int i;
-	for (i = 0; i < MAX_NUM_OF_THROWS; i++)
-	knockDown(0);
+	rollMany(MAX_NUM_OF_THROWS, 0);
 	TEST_ASSERT_EQUAL(0, score());
 }
 
 // Drugi test - srusen jedan cunj u svim bacanjima
 TEST(BowlingTest, TestAllOnes)
 {
-	int i;
-	for (i = 0; i < MAX_NUM_OF_THROWS; i++)
-	knockDown(1);
+	rollMany(MAX_NUM_OF_THROWS, 1);
 	TEST_ASSERT_EQUAL(21, score());
 }
 
 // Treci test - spare u drugom bacanju (5+5), ostala bacanja po 1 cunj 
 TEST(BowlingTest, TestSpare)
 {
-	int i;
 	knockDown(5);
 	knockDown(5);
-	for (i = 0; i < MAX_NUM_OF_THROWS - 2; i++)
-	knockDown(1);
+	rollMany(MAX_NUM_OF_THROWS - 2, 1);
 	TEST_ASSERT_EQUAL(30, score());
 }
 
 // Cetvrti test - strike u prvom bacanju (10), ostala bacanja po 1 cunj 
 TEST(BowlingTest, TestStrike)
 {
-	int i;
 	knockDown(10);
-	for (i = 0; i < MAX_NUM_OF_THROWS - 2; i++)
-	knockDown(1);
+	rollMany(MAX_NUM_OF_THROWS - 2, 1);
 	TEST_ASSERT_EQUAL(31, score());
 }
 
 // Peti test - strike u prvom i drugom bacanju, ostala bacanja po 1 cunj 
 TEST(BowlingTest, TwoStrikesInRow)
 {
-	int i;
 	knockDown(10);
 	knockDown(10);
-	for (i = 0; i < MAX_NUM_OF_THROWS - 4; i++)
-	knockDown(1);
+	rollMany(MAX_NUM_OF_THROWS - 4, 1);
 	TEST_ASSERT_EQUAL(50, score());
 }
 // Sesti test -strike u svakom bacanju(svaki put po 10 poena)
 TEST(BowlingTest, TenStrikesInRow)
 {
-	int i;
-	for(i = 0; i < MAX_NUM_OF_THROWS-9; i++)
-	{
-	 knockDown(10);	
-	}
+	rollMany(MAX_NUM_OF_THROWS - 9, 10);
  	TEST_ASSERT_EQUAL(300, score());
 }
 
 //Sedmi test - poslednje bacanje strike 
 TEST(BowlingTest, LastStrike)
 {
-	int i;
-	for(i = 0; i < MAX_NUM_OF_THROWS - 3; i++)
-	{ 
-	 knockDown(0);	
-	}
+	rollMany(MAX_NUM_OF_THROWS - 3, 0);
 	knockDown(1);
 	knockDown(9);
 	knockDown(10);
@@ -107,16 +96,14 @@ TEST(BowlingTest, LastStrike)
 
 TEST(BowlingTest, SparesAndStrikes)
 {
-int i;
 	knockDown(10);
 	knockDown(10);
 	knockDown(5);
 	knockDown(5);
 	knockDown(6);
 	knockDown(4);
-	for(i=0; i < MAX_NUM_OF_THROWS - 8; i++)
-	knockDown(1);
-	TEST_ASSERT_EQUAL(85,score());
+	rollMany(MAX_NUM_OF_THROWS - 8, 1);
+	TEST_ASSERT_EQUAL(85, score());
 
 }
 // Deveti test - bez strikeova i spareova
@@ -129,11 +116,7 @@ TEST(BowlingTest, NoStrikesNoSpares)
 	knockDown(4);
 	knockDown(5);
 	knockDown(1);
-	int i;
-	for(i=0;i<MAX_NUM_OF_THROWS - 6; i++)
-	knockDown(2);
-
-
+	rollMany(MAX_NUM_OF_THROWS - 6, 2);
 	TEST_ASSERT_EQUAL(52,score());
 }
 //Deseti test - prvo bacanje u svakom frejmu promasaj
@@ -143,11 +126,11 @@ TEST(BowlingTest, FirstAttemptAlwaysMiss)
 	int i = 0;
 	do
 	{
-	if(i%2==0)
+	if(i % 2 == 0)
 	knockDown(0);
 	else knockDown(1);
 	i++;
-	}while(i<MAX_NUM_OF_THROWS-1);
+	}while(i < MAX_NUM_OF_THROWS-1);
 	knockDown(1);
 	
 	TEST_ASSERT_EQUAL(11,score());
@@ -156,14 +139,10 @@ TEST(BowlingTest, FirstAttemptAlwaysMiss)
 TEST(BowlingTest, UnallowedMemoryAccess)
 {
 	int pointsCheck[24]={};
-	int i;
-	for(i = 0; i < MAX_NUM_OF_THROWS-3; i++)
-	{
-	 knockDown(0);	
-	}
-	 knockDown(10);	
-	 knockDown(10);	
-	 knockDown(10);	
+	rollMany(MAX_NUM_OF_THROWS - 3, 0);
+	knockDown(10);	
+	knockDown(10);	
+	knockDown(10);	
 	testMemory(pointsCheck);
  	TEST_ASSERT_EQUAL(0, pointsCheck[22]);
 }
