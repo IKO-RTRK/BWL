065a468df664ef73084539d996041292177c1e80
Stefan Savić <stephonsavic@gmail.com>
2015-09-04 14:24:13 +0200

    Sada moze proizvoljno traka i tabela [ 0-3 ]

--- frontend/src/bowling_gui.h
+++ frontend/src/bowling_gui.h
@@ -2,21 +2,22 @@
 #define BOWLING_GUI_H
 
 #define ROW 34
-#define COLUMN 63
+#define COLUMN 200
 #define NUM_OF_PINS 10
 #define BALL_POS_ROW 33
 #define BALL_POS_COL 8
 #define START_LANE_ROW 0
 #define START_LANE_COLON 0
 #define START_PINS 5
+#define DIFF 45
+#define DIFF_TABLES 45
 #define min(a,b) (((a)<=(b))?(a):(b))
 #define START_OF_FIRST_TRACK_ROW 5
 #define END_OF_PINS_ROW 9
 
 
-void initialisationLane(void);
-void initialisationPins(void);
-void initialisationTable(void);
+void initialisationTable(int TrackNumber);
+void initialisationTrack(int TrackNumber);
 void print(void);
 void move(void);
 void pinsDown(int);
